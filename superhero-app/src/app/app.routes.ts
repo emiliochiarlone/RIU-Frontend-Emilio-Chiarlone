360be0ba8fa14c6ee49fc8595ba2197f3fdf91a2
emilio <emiliochiarlone2008@hotmail.com>
2025-07-29 19:23:42 -0300

    Routes and app configuration

--- superhero-app/src/app/app.routes.ts
+++ superhero-app/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'heroes',
+    loadComponent: () => import('./heroes/hero-list/hero-list.component').then(m => m.HeroListComponent)
+  }
+];
