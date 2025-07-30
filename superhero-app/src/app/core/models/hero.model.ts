88f5449ae31a1b2589a7d6abb64014db69b6b5b6
emilio <emiliochiarlone2008@hotmail.com>
2025-07-30 17:40:51 -0300

    feat: Created Hero model, mocked heroes list and errorCodes

--- superhero-app/src/app/core/models/hero.model.ts
+++ superhero-app/src/app/core/models/hero.model.ts
@@ -1,6 +1,27 @@
 export class Hero {
+  static currentId = 1;
+
   constructor(
-    public id: number,
-    public name: string
-  ) {}
+    private _name: string,
+    private _id?: number
+  ) {
+    this._id = _id !== undefined ? _id : Hero.currentId++;
+    this._name = _name;
+  }
+
+  set name(newName: string) {
+    this._name = newName;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get id(): number {
+    return this._id || 0;
+  }
+
+  toString(): string {
+    return `Hero ${this.id}: ${this.name}`;
+  }
 }
---
+++ superhero-app/src/app/core/models/mockedHeroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from "@core/models/hero.model";
+
+export const mockedHeroNames: string[] = [
+  'Spiderman',
+  'Batman',
+  'Superman',
+  'Ironman',
+  'Capitán América',
+  'Mujer Maravilla',
+  'Flash',
+  'Thor',
+  'Hulk',
+  'Black Widow',
+  'Doctor Strange',
+  'Deadpool'
+];
