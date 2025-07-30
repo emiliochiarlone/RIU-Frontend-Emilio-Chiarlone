8df8a0e466ba66edbeee9e287d45b6dd7a77b9f3
emilio <emiliochiarlone2008@hotmail.com>
2025-07-30 17:41:18 -0300

    feat: HeroService implementation and Unit test

--- superhero-app/src/app/core/services/heroes/hero.service.spec.ts
+++ superhero-app/src/app/core/services/heroes/hero.service.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 
-import { HeroService } from '../hero.service';
+import { HeroService } from './hero.service';
+import { Hero } from '@core/models/hero.model';
+import { ErrorCodes } from '@core/utils/errorcodes';
 
 describe('HeroService', () => {
   let service: HeroService;
@@ -10,7 +12,205 @@ describe('HeroService', () => {
     service = TestBed.inject(HeroService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    service.resetToInitialState();
+  });
+
+  describe('service creation', () => {
+    it('should be created', () => {
+      expect(service).toBeTruthy();
+    });
+
+    it('should initialize with mock heroes', () => {
+      service.getAll().subscribe((heroes: Hero[]) => {
+        expect(heroes.length).toBeGreaterThan(0);
+        expect(heroes[0]).toBeInstanceOf(Hero);
+      });
+    });
+
+    it('should fill with mock heroes', () => {
+      const heroes = service.fillWithMockHeroes();
+      expect(heroes.length).toBeGreaterThan(0);
+      expect(heroes[0]).toBeInstanceOf(Hero);
+    });
+  });
+
+  describe('CRUD operations', () => {
+    it('should return all heroes', (done) => {
+      service.getAll().subscribe((heroes: Hero[]) => {
+        expect([...heroes]).toBeInstanceOf(Array);
+        done();
+      });
+    });
+
+    it('should create a hero', (done) => {
+      const newHero = new Hero('NewName');
+      service.create(newHero).subscribe((createdHero: Hero) => {
+        service.getAll().subscribe((heroes: Hero[]) => {
+          expect(heroes).toContain(
+            jasmine.objectContaining({
+              id: newHero.id,
+              name: newHero.name,
+            })
+          );
+          done();
+        });
+      });
+    });
+
+    it('should delete a hero', (done) => {
+      service.getAll().subscribe((heroes: Hero[]) => {
+        service.delete(heroes[0].id).subscribe((deletedId: number) => {
+          expect(deletedId).toBe(heroes[0].id);
+          done();
+        });
+      });
+    });
+
+    it('should update a hero', (done) => {
+      service.getAll().subscribe((heroes: Hero[]) => {
+        const updatedHero = new Hero('Updated Hero', heroes[0].id);
+        service.update(updatedHero).subscribe((hero: Hero) => {
+          expect(hero).toEqual(updatedHero);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('error handling', () => {
+    it('Should handle duplicate ID error', (done) => {
+      service.getAll().subscribe((heroes: Hero[]) => {
+        const duplicatedIdHero = new Hero('Duplicated Hero', heroes[0].id);
+
+        service.create(duplicatedIdHero).subscribe({
+          next: () => {
+            fail(ErrorCodes.DUPLICATE_ID + ' was expected');
+            done();
+          },
+          error: (error) => {
+            expect(error.code).toBe(ErrorCodes.DUPLICATE_ID);
+            done();
+          },
+        });
+      });
+    });
+
+    it('should handle duplicate name error', (done) => {
+      service.getAll().subscribe((heroes: Hero[]) => {
+        const duplicatedNameHero = new Hero(heroes[0].name);
+        service.create(duplicatedNameHero).subscribe({
+          next: () => {
+            fail(ErrorCodes.DUPLICATE_NAME + ' was expected');
+            done();
+          },
+          error: (error) => {
+            expect(error.code).toBe(ErrorCodes.DUPLICATE_NAME);
+            done();
+          },
+        });
+      });
+    });
+
+    it('should handle hero not found error on delete', (done) => {
+      service.delete(999).subscribe({
+        next: () => {
+          fail(ErrorCodes.HERO_NOT_FOUND + ' was expected');
+          done();
+        },
+        error: (error) => {
+          expect(error.code).toBe(ErrorCodes.HERO_NOT_FOUND);
+          done();
+        },
+      });
+    });
+
+    it('should handle hero not found error on update', (done) => {
+      const nonExistentHero = new Hero('non existing hero', 999);
+      service.update(nonExistentHero).subscribe({
+        next: () => {
+          fail(ErrorCodes.HERO_NOT_FOUND + ' was expected');
+          done();
+        },
+        error: (error) => {
+          expect(error.code).toBe(ErrorCodes.HERO_NOT_FOUND);
+          done();
+        },
+      });
+    });
+
+    it('should handle hero not fund error on find by name', (done) => {
+      service.findByName('No existing hero').subscribe((heroes: Hero[]) => {
+        expect(heroes.length).toBe(0);
+        done();
+      });
+    });
+
+    it('should return empty array for out of range page', (done) => {
+      service.getAllPaginated(999, 5).subscribe((heroes: Hero[]) => {
+        expect(heroes.length).toBe(0);
+        done();
+      });
+    });
+  });
+
+  describe('pagination', () => {
+    it('should return paginated heroes', (done) => {
+      service.getAll().subscribe((heroes: Hero[]) => {
+        service.getAllPaginated(1, 5).subscribe((paginatedHeroes: Hero[]) => {
+          expect(paginatedHeroes.length).toBe(heroes.splice(0, 5).length);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('find by name', () => {
+    it('should find heros by name', (done) => {
+      service.getAll().subscribe((heroes: Hero[]) => {
+        const heroName = heroes[0].name;
+        service.findByName(heroName).subscribe((foundHeroes: Hero[]) => {
+          expect(foundHeroes[0].name).toBe(heroName);
+          done();
+        });
+      });
+    });
+
+    it('should return all heroes if name is empty', (done) => {
+      service.getAll().subscribe((heroes: Hero[]) => {
+        service.findByName('').subscribe((heroes: Hero[]) => {
+          expect(heroes.length).toBeGreaterThan(0);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('utility methods', () => {
+    it('should check hero Id exists', () => {
+      const hero = new Hero('New hero');
+      service.create(hero).subscribe();
+
+      const idExists = service.idAlreadyExists(hero.id);
+      expect(idExists).toBeTrue();
+    });
+
+    it ('should identify non existing Id', () => {
+      const result = service.idAlreadyExists(999);
+      expect(result).toBe(false);
+    });
+
+    it('should check if hero name exists', () => {
+      const hero = new Hero('new hero');
+      service.create(hero).subscribe();
+
+      const exists = service.nameAlreadyExists(hero.name);
+      expect(exists).toBeTrue();
+    });
+
+    it('should identify non existing name', () => {
+      const result = service.nameAlreadyExists('Non existing hero');
+      expect(result).toBe(false);
+    });
   });
 });
--- superhero-app/src/app/core/services/heroes/hero.service.ts
+++ superhero-app/src/app/core/services/heroes/hero.service.ts
@@ -1,9 +1,112 @@
 import { Injectable } from '@angular/core';
+import { mockedHeroNames } from '../../models/mockedHeroes';
+import { Hero } from '@core/models/hero.model';
+import { Observable, of, throwError } from 'rxjs';
+import { ErrorCodes } from '@core/utils/errorcodes';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeroService {
+  private _heroes: Hero[] = [];
 
-  constructor() { }
+  constructor() {
+    this.fillWithMockHeroes();
+  }
+
+  fillWithMockHeroes(): Hero[] {
+    this._heroes = mockedHeroNames.map((name: string) => new Hero(name));
+    return [...this._heroes];
+  }
+
+  create(hero: Hero): Observable<Hero> {
+    if (this.idAlreadyExists(hero.id)) {
+      return throwError(() => ({
+        code: ErrorCodes.DUPLICATE_ID,
+        message: 'Ya existe un héroe con este ID',
+      }));
+    } else if (this.nameAlreadyExists(hero.name)) {
+      return throwError(() => ({
+        code: ErrorCodes.DUPLICATE_NAME,
+        message: 'Ya existe un héro con este nombre',
+      }));
+    }
+    this._heroes.push(hero);
+    return of(hero);
+  }
+
+  delete(id: number): Observable<number> {
+    const index = this._heroes.findIndex((h) => h.id === id);
+    if (index === -1) {
+      return throwError(
+        () => ({
+          code: ErrorCodes.HERO_NOT_FOUND,
+          message: 'No se encontro el héroe con el Id proporcionado.',
+        })
+      );
+    }
+    this._heroes.splice(index, 1);
+    return of(id);
+  }
+
+  getAll(): Observable<Hero[]> {
+    return of([...this._heroes]);
+  }
+
+  getAllPaginated(page: number, pageSize: number): Observable<Hero[]> {
+    const start = (page - 1) * pageSize;
+    const end = start + pageSize;
+    const paginatedHeroes = this._heroes.slice(start, end);
+    return of(paginatedHeroes);
+  }
+
+  findByName(name: string): Observable<Hero[]> {
+    if (!name) {
+      return of([...this._heroes]);
+    }
+    let foundHeroes = this._heroes.filter((hero: Hero) =>
+      hero.name.toLowerCase().includes(name.toLowerCase())
+    );
+    return of(foundHeroes);
+  }
+
+  update(hero: Hero): Observable<Hero> {
+    const index = this._heroes.findIndex((h) => h.id === hero.id);
+    if (index === -1) {
+      return throwError(
+        () => ({
+          code: ErrorCodes.HERO_NOT_FOUND,
+          message: 'No se encontro el héroe con el Id proporcionado.'
+        })
+      );
+    } else {
+      if (this.nameAlreadyExists(hero.name)) {
+        return throwError(() => ({
+          code: ErrorCodes.DUPLICATE_NAME,
+          message: 'Ya existe un héroe con este nombre',
+        }));
+      }
+    }
+    this._heroes[index] = hero;
+    return of(hero);
+  }
+
+  resetToInitialState(): void {
+    this.fillWithMockHeroes();
+  }
+
+  idAlreadyExists(id: number): boolean {
+    return this._heroes.some((hero: Hero) => hero.id === id);
+  }
+
+  nameAlreadyExists(name: string): boolean {
+    return this._heroes.some(
+      (hero: Hero) => hero.name.toLowerCase() === name.toLowerCase()
+    );
+  }
+
+  get heroes(): Hero[] {
+    return this._heroes;
+  }
+
 }
