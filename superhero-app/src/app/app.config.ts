57f01b017dd7b961554eff9ee256f6018100e92e
emilio <emiliochiarlone2008@hotmail.com>
2025-08-01 17:32:27 -0300

    chore: Updated App config and Routes

--- superhero-app/src/app/app.config.ts
+++ superhero-app/src/app/app.config.ts
@@ -2,10 +2,19 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-
+import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { LoadingInterceptor } from '@core/interceptors/loading.interceptor';
+import { withInterceptorsFromDi } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoadingInterceptor,
+      multi: true
+    },
   ]
 };
--- superhero-app/src/app/app.routes.ts
+++ superhero-app/src/app/app.routes.ts
@@ -3,6 +3,27 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'heroes',
+    data: { title: 'Lista de Heroes' },
     loadComponent: () => import('./heroes/hero-list/hero-list.component').then(m => m.HeroListComponent)
+  },
+  {
+    path: 'heroes/edit/:id',
+    data: { title: 'Editar Héroe' },
+    loadComponent: () => import('./heroes/hero-form/hero-form.component').then(m => m.HeroFormComponent)
+  },
+  {
+    path: 'heroes/create',
+    data: { title: 'Crear Héroe' },
+    loadComponent: () => import('./heroes/hero-form/hero-form.component').then(m => m.HeroFormComponent)
+  },
+  {
+    path:'',
+    redirectTo: 'heroes',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'heroes',
+    pathMatch: 'full'
   }
 ];
