eaf32680e513847a29b8d5dc15dfb5ab974a7d22
emilio <emiliochiarlone2008@hotmail.com>
2025-08-01 17:33:45 -0300

    feat: Implemented Http loader managed by interceptor and service

---
+++ superhero-app/src/app/core/interceptors/loading.interceptor.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpResponse,
+} from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { delay, finalize } from 'rxjs/operators';
+import { LoadingService } from '../services/loading.service';
+
+@Injectable()
+export class LoadingInterceptor implements HttpInterceptor {
+  constructor(private loadingService: LoadingService) {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    
+    this.loadingService.startLoading();
+
+
+    return next
+      .handle(req)
+      .pipe(
+        delay(1000), 
+        finalize(() => this.loadingService.stopLoading()));
+  }
+}
