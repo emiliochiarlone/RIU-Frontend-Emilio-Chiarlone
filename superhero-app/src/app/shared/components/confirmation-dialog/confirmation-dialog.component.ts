020c3b9a161f33f53a7ac8f144ba4ed9dd3b6493
emilio <emiliochiarlone2008@hotmail.com>
2025-08-01 17:46:45 -0300

    feat: ConfirmationDialog Component Implementation

--- superhero-app/src/app/shared/components/confirmation-dialog/confirmation-dialog.component.ts
+++ superhero-app/src/app/shared/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MaterialModule } from '@shared/material/material.module';
+
+
+export interface ConfirmationDialogData {
+  title: string;
+  message: string;
+  confirmText?: string;
+  cancelText?: string;
+}
 
 @Component({
   selector: 'app-confirmation-dialog',
-  imports: [],
+  imports: [MaterialModule,CommonModule],
   templateUrl: './confirmation-dialog.component.html',
   styleUrl: './confirmation-dialog.component.scss'
 })
-export class ConfirmationDialogComponent {
 
+
+export class ConfirmationDialogComponent {
+  dialogRef: MatDialogRef<ConfirmationDialogComponent> = inject(MatDialogRef<ConfirmationDialogComponent>);
+
+  constructor(
+  @Inject(MAT_DIALOG_DATA) public data: ConfirmationDialogData
+  ) {
+    this.data.confirmText = this.data.confirmText || 'Confirmar';
+    this.data.cancelText = this.data.cancelText || 'Cancelar';
+  }
+
+  onConfirm(): void {
+    this.dialogRef.close(true);
+  }
+  onCancel(): void {
+    this.dialogRef.close(false);
+  }
 }
