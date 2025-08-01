5bd5a9704dc112512de89c0f4d376cc5393e92ef
emilio <emiliochiarlone2008@hotmail.com>
2025-08-01 17:30:33 -0300

    feat: Implemented App Component. Welcome methods and layout styles

--- superhero-app/src/app/app.component.ts
+++ superhero-app/src/app/app.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '@shared/components/header/header.component';
+import { CommonModule } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+import { WelcomeComponent } from '@shared/components/welcome/welcome.component';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'superhero-app';
+  dialog = inject(MatDialog);
+  welcomeAlreadyShown = false;
+  welcomeDialogSubscription!: Subscription;
+
+  ngOnInit(): void {
+    this.welcomeAlreadyShown = localStorage.getItem('welcomeShown') === 'true';
+    if (!this.welcomeAlreadyShown) this.showWelcome();
+  }
+
+  showWelcome(): void {
+    if (!this.welcomeAlreadyShown) {
+      this.dialog
+        .open(WelcomeComponent).afterClosed().subscribe(() => {
+        this.setWelcomeShown();
+      });
+    }
+  }
+
+  setWelcomeShown(): void {
+    localStorage.setItem('welcomeShown', 'true');
+    this.welcomeAlreadyShown = true;
+    this.welcomeDialogSubscription?.unsubscribe();
+  }
 }
